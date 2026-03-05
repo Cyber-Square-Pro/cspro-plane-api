959b3de9de363d3b1ddc46e39c98bd0966c65186
Christian Hope <2017117171@ub.edu.bz>
2026-03-04 22:08:05 -0600

    Made the landing page look more appealing by providing sonme css styling. also created some links to swagger, django admin page and the github documentation repository for a better ease of access

--- Plane/views.py
+++ Plane/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def landing_page(request):
-    return HttpResponse("<h1>Welcome to the Plane API Landing Page</h1><p>This is the root endpoint.</p>")
+    return render(request, 'landing.html')
